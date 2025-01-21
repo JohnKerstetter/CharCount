0d6d2b0e96865bab45298add9e33f296985b66f0
John Kerstetter <johnkerstetter0@gmail.com>
2025-01-20 19:37:12 -0800

    create repository

---
+++ CharCount.java
@@ -0,0 +1,40 @@
+// Name: John Kerstetter
+// Class: CS 240
+// Assignment: Lab 2 - Character Count
+// File: CharCount.java
+
+import java.util.Scanner;
+
+public class CharCount {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String inputString = input.nextLine();
+
+        System.out.print("Enter char: ");
+        char inputChar = input.next().charAt(0);
+
+        int c = count(inputString.toCharArray(), inputChar);
+        System.out.println("Counted chars: " + c);
+
+        input.close();
+    }
+
+    // counts amount of times ch appears in chars
+    public static int count(char[] chars, char ch) {
+        return count(chars, ch, 0);
+    }
+
+    // helper method
+    public static int count(char[] chars, char ch, int high) {
+        if (high >= chars.length) {
+            return 0;
+        } else if (chars[high] == ch) {
+            return count(chars, ch, high + 1) + 1; 
+        }
+        else {
+            return count(chars, ch, high + 1);
+        }
+    }
+}
